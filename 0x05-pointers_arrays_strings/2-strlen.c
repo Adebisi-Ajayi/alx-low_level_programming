7a8b3841d69c7247692531b092ed189655ff7665
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-18 01:29:06 -0700

    I do not fear computers.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a strong
+ * @s: string
+ * Return: returns lenght;
+ */
+
+int _strlen(char *s)
+{
+	int ade = 0;
+
+	while (*s != '\0')
+	{
+		ade++;
+		s++;
+	}
+	retun ade;
+}
