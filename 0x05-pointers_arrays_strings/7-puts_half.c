73c52ec656641e5940150c40b435ae9be526d924
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-18 08:32:16 -0700

    Winning is the only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,19 @@
  */
 void puts_half(char *str)
 {
-	int len = _strlen(str);
-
-	if (len % 2 == 0)
-		len = len / 2;
-	else
-		len = (len + 1) / 2;
+	int len = 0, i, n;
 
 	while (str[len] != '\0')
-	{
-		_putchar(str[len]);
 		len++;
-	}
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
