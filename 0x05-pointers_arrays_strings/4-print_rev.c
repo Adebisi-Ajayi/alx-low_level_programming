da5fede3c8647a08c01756a222c135bd41d51894
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-18 02:09:22 -0700

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 	int ade = 0;
 
-	while (str[ade] != '\0')
+	while (s[ade] != '\0')
 	{
 		ade++;
 	}
 	for (int i = ade - 1; i >= 0; i--)
 	{
-		putchar(str[i]);
+		putchar(s[i]);
 	}
 	putchar('\n');
 }
