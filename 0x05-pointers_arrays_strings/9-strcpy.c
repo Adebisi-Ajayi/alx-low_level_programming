bde5ae41b0f82962d68137a8585042930ae9b76c
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-18 09:20:28 -0700

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strcpy - a function that copies the string pointed to by src,
  * including the terminating null bytes (\0),
  * to the buffer pointed to by dest.
  *
- * @dest: a pointer to destination of string
- * @src: a pointer to source string to copy from
+ * @dest: A pointer to destination of string
+ * @src: A pointer to source string to copy from
  *
- * return: pointer to dest
+ * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
