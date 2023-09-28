f1d6fc099bc0fefd81991f20f04d2434104db076
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-28 04:59:20 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,63 +8,22 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
 	if (n == 0 || n == 1)
-	{
 		return (n);
-	}
-	return (1, n / 2, n);
+	return (_sqrt(0, n));
 }
 
 /**
  *_sqrt - return the square root
- *@low: test number
- *@n: square number
-*@high: test the two
+ *@n: test number
+ *@x: square number
  *Return: the square root of n
  */
-int _sqrt(int low, int high, int n)
+int _sqrt(int n, int x)
 {
-	if (low <= high)
-	{
-		int mid = low + (high - low) / 2;
-		int square = mid * mid;
-
-		if (square == n)
-		{
-			return (mid);
-		}
-		else if (square < n)
-		{
-			return (_sqrt(mid + 1, high, n));
-		}
-		else
-		{
-			return (_sqrt(low, mid - 1, n));
-		}
-	}
-	return (-1);
-}
-/**
- * main - print the root
- * Return: the root
- */
-int main(void)
-{
-	int n = 25;
-
-	int result = _sqrt_recursion(n);
-
-	if (result == -1)
-	{
-		printf("No natuural square root or %d\n", n);
-	}
-	else
-	{
-		printf("The natural square root of %d is %d\n", n, result);
-	}
-	return (0);
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
