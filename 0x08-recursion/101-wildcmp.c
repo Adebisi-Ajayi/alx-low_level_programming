2cd5e7f8c26b31925182847668f1df7a429e83df
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-29 05:43:52 -0700

    Inception. Now, before you bother telling me it is impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,13 +7,30 @@
  *
  * Return: 1 if identical, 0 otherwise
  */
+int wildcmp_recursive(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		if (wildcmp_recursive(s1 + 1, s2 + 1) || wildcmp_recursive(s1, s2 + 1))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * wildcmp - compares two strings and return 1 if identical
+ * @s1: string to be checked
+ * @s2: pattern to be used
+ *
+ * Return: 1 if identical, 0 otherwise
+ */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s2 != '\0' && wildcmp(s1 + 1, s2)));
-	return (0);
+	return wildcmp_recursive(s1, s2);
 }
