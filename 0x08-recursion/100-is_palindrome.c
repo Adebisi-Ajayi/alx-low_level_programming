f0663579daeecb5115f2b2b12f34090fcd0909b1
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-09-29 03:43:12 -0700

    They say we only use a fraction of our brain is true potential

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * is_palindrome - print the word in reverse order
  * @s: check the word in reverse order
@@ -8,14 +8,16 @@
 
 int is_palindrome(char *s)
 {
-	if (strlen(s) <= 1)
+	int len = strlen(s);
+
+	if (len <= 1)
 	{
 		return (1);
 	}
-	if (s[0] != s[strlen(s) - 1])
+	if (s[0] != s[len - 1])
 	{
 		return (0);
 	}
-	s[strlen(s) - 1] = '\0';
+	s[len - 1] = '\0';
 	return (is_palindrome(s + 1));
 }
