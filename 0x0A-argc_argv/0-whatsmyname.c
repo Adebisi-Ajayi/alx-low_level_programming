03cf7a61b60434839f353a1dbb137d0dba1b62e8
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-02 04:09:36 -0700

    It are not what they call you, it is what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0 && argv[0] != NULL)
