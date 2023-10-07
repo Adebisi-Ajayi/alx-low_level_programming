366755b438ebe02a0f450ca618f20845c7321fd5
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-06 23:33:57 -0700

    Functions that allocate memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - to check for unsigned value
+ * @b: value of b
+ * Return: return int
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
