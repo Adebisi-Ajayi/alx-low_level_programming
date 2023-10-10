581b4c2b6209f26f47ffd48dc1241622b15633b8
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-10 05:33:29 -0700

    Sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,15 +1,4 @@
-#include <stdio.h>
-/**
- * main - print function
- *
- * Return: return main
- */
-int main(void)
-{
-	int x = 32;
-	int y = 8;
-	int res = SUM(x, y);
-
-	printf("The sum of %d and %d is %d\n", x, y, res);
-	return (0);
-}
+#ifndef _SUM_H
+#define _SUM_H
+#define SUM(x, y) ((x) + (y))
+#endif
