3ba0e948dabc5435cf07ce90b659614aff0aa9c2
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-23 12:12:15 -0700

    correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,37 +7,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slower, *faster;
-	size_t count = 0;
+	size_t nodes = 0;
+	const listint_t *one = head, *two = head;
 
-	slower = head;
-	faster = head;
+	if (head == NULL)
+		exit(98);
 
-	while (slower && faster && faster->next)
+	while (one && two && two->next && head)
 	{
-		printf("%d\n", slower->n);
-		slower = slower->next;
-		faster = faster->next->next;
-
-		if (slower == faster)
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
 		{
-			printf("-> [%p] %d\n", (void *)faster, faster->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			exit(98);
 		}
-	}
-	if (!faster)
-	{
-		return (0);
-	}
 
-/*	size_t count = 0; */
-
-	while (slower != NULL)
-	{
-		printf("%d\n", slower->n);
-		slower = slower->next;
-		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
 	}
-	return (count);
-
+	head = NULL;
+	return (nodes);
 }
