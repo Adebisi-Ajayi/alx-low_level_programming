efd1e52e7746fe9278acdc02a9cb33df94f917ed
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-25 23:43:51 -0700

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_bit - the bit to get
+ * @n: value to get
+ * @index: the array of inde
+ * Return: nothing
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int elk;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (1);
+	}
+
+	elk = 1UL << index;
+
+	if ((n & elk) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
