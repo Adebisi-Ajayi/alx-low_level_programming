8109db51952bcf6058c58c26fd3f17cefa050680
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-26 00:00:38 -0700

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * clear_bit - the bit to clear
+ * @n: the value to be cleared
+ * @index: the array to be cleared
+ * Return: nothing
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int elk;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (1);
+	}
+
+	elk = ~(1UL << index);
+	*n = (*n & elk);
+	return (1);
+}
