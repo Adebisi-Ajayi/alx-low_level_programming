79ed9c1ef41eacfe2d292594cf879f19875a213f
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-10-25 23:51:24 -0700

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * set_bit - the bit to set
+ * @n: the value to set
+ * @index: the array to set
+ * Return: nothing
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int elk;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (1);
+	}
+
+	elk = 1UL << index;
+
+	*n = (*n | elk);
+	return (1);
+}
